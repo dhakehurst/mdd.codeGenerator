ea87e0cac262bc3852cf685a5a6992ffd0b26495
David Akehurst <dave@akehurst.net>
2014-02-22 19:04:18 +0000

    add

---
+++ framework/develop/mdd.framework/src/main/csharp/framework/collections/Set.cs
@@ -0,0 +1,23 @@
+/*************************************************************************
+* Copyright (c) 2013 - 2014 Dr David H. Akehurst.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* 
+*************************************************************************/
+﻿namespace framework.collections
+{
+    using framework.basicTypes;
+    using System.Linq;
+
+    /// <summary>
+    /// An implementation of the Sequence type from
+    /// the OMG OCL standard
+    /// </summary>
+    public interface Set<T> : Bag<T>
+    {
+        Boolean isSubsetOf(Set<T> other);
+    }
+}
